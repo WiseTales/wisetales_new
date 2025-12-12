6fd9bcd02c20bc1909c6dbb3e8f3c81db2b51338
WiseTales <zuhairtoons@gmail.com>
2025-12-12 20:39:53 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import Index from "./pages/Index";
 import About from "./pages/About";
 import Watch from "./pages/Watch";
 import Parents from "./pages/Parents";
-import GetInvolved from "./pages/GetInvolved";
 import Donate from "./pages/Donate";
 import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import AIAdsPage from "./pages/aiAdStudio"; // Added AI Ads page
 
 const queryClient = new QueryClient();
 
@@ -26,11 +26,11 @@ const App = () => (
           <Route path="/about" element={<About />} />
           <Route path="/watch" element={<Watch />} />
           <Route path="/parents" element={<Parents />} />
-          <Route path="/get-involved" element={<GetInvolved />} />
+          <Route path="/aiAdStudio" element={<AIAdsPage />} /> {/* Added route */}
           <Route path="/donate" element={<Donate />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* Catch-all for 404 */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
